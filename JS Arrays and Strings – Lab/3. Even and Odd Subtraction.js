0d57bdc6947f325cec0317c62405f266ba6dab51
xaoccc <114498517+xaoccc@users.noreply.github.com>
2024-07-01 23:16:59 +0300

    js problems

--- JS Arrays and Strings – Lab/3. Even and Odd Subtraction.js
+++ JS Arrays and Strings – Lab/3. Even and Odd Subtraction.js
@@ -1,12 +1,20 @@
 function evenAndOddSubtraction(arr) {
-    let evenSum = 0;
-    let oddSum = 0;
-    for(let num of arr){
-        if(num % 2 === 0){
-            evenSum += num;
-        } else {
-            oddSum += num;
-        }
-    }
-    console.log(evenSum - oddSum);
+    // let evenSum = 0;
+    // let oddSum = 0;
+    // for(let num of arr){
+    //     if(num % 2 === 0){
+    //         evenSum += num;
+    //     } else {
+    //         oddSum += num;
+    //     }
+    // }
+    // console.log(evenSum - oddSum);
+
+    let evenSum = arr.filter((num) => num % 2 === 0)
+        .reduce((a, b) => a + b, 0);
+
+    console.log((2 * evenSum) - arr.reduce((a, b) => a + b, 0));
 }
+
+
+evenAndOddSubtraction([1, 2, 3, 4, 5, 10]); // 3
