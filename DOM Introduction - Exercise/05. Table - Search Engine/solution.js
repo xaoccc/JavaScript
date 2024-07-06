3247e487fefae672a46982c6251285f989840e7f
xaoccc <114498517+xaoccc@users.noreply.github.com>
2024-07-06 13:48:51 +0300

    js problems

--- DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,21 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+
+      let searchPattern = document.querySelector('#searchField').value;
+      let regex = new RegExp(searchPattern, 'i');
+      let data = document.querySelectorAll('td');
+      let rows = document.querySelectorAll('tr');
+
+      for (let row of rows) {         
+         row.className = '';         
+      }
+
+      for (let item of data) {
+         if (regex.test(item.textContent)) {
+            item.parentElement.className = 'select';
+         } 
+      }
 
    }
 }
