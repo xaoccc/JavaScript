d68f1941f6985bc1a0b0b18e48c83adfbff9c32e
xaoccc <114498517+xaoccc@users.noreply.github.com>
2024-07-07 21:47:35 +0300

    jiwdsujhqawiud

--- DOM Introduction - Exercise/06. Format the Text/solution.js
+++ DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,17 @@
-function solve() {
-  //TODO
+function fortmatTheText() {
+  
+    let text = document.getElementById('input').value;
+    let sentences = text.split('.').filter(x => x !== '');
+    let result = '';
+  
+    for (let i = 0; i < sentences.length; i++) {
+      let sentence = sentences[i].trim();
+      result += sentence + '.';
+      if (sentence.length > 0) {
+        result += ' ';
+      }
+    }
+
+  
+    document.getElementById('output').textContent = result;
 }
