5a3f27546f93963449865a3e533cf92898314359
xaoccc <xaocccc@gmail.com>
2024-07-08 10:48:53 +0300

    js dom exercise

--- DOM Introduction - Exercise/06. Format the Text/solution.js
+++ DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,17 +1,28 @@
-function fortmatTheText() {
+function solve() {
+    // Reset the output
+    document.getElementById('output').innerHTML = '';
   
     let text = document.getElementById('input').value;
     let sentences = text.split('.').filter(x => x !== '');
-    let result = '';
-  
-    for (let i = 0; i < sentences.length; i++) {
-      let sentence = sentences[i].trim();
-      result += sentence + '.';
-      if (sentence.length > 0) {
-        result += ' ';
+    let result = [];
+    let paragraph = document.createElement("p");
+
+      for (let i = 1; i < sentences.length + 1; i++) {
+      let sentence = sentences[i - 1].trim();      
+      paragraph.textContent += sentence + '.';      
+      
+      if (i % 3 === 0) {
+        result.push(paragraph);
+        paragraph = document.createElement("p");        
       }
     }
+    // If paragraphs number is not divisible by 3, it means that there is one paragraph left and we need to add it to the result
+    if (paragraph.textContent !== '') {
+      result.push(paragraph);
+    }
 
-  
-    document.getElementById('output').textContent = result;
+    for (par of result) {
+      document.getElementById('output').appendChild(par);
+    }    
+    
 }
