74a685df33ea16c84fb17ce534d97e977495d218
xaoccc <xaocccc@gmail.com>
2024-07-08 17:00:20 +0300

    js dom exercise

--- DOM Introduction - Exercise/08. Generate Report/report.js
+++ DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,29 @@
 function generateReport() {
-    //TODO
+    let checked = document.querySelectorAll('th input');
+    let tbody = document.querySelectorAll('tbody');
+    let checkedHeadersIndexes = {};
+    let result = []
+
+    for (c of checked) {
+        if (c.checked) {
+
+            checkedHeadersIndexes[Array.from(c.parentNode.parentNode.children).indexOf(c.parentNode)] = c.parentNode.textContent.toLowerCase().trim();
+        };
+    }
+    console.log(checkedHeadersIndexes);
+    for (row of tbody) {
+        for (col of Array.from(row.children)){
+            let rowData = {}
+            for (data of col.children) {
+                if (Object.keys(checkedHeadersIndexes).includes(String(Array.from(col.children).indexOf(data)))) {
+                    rowData[checkedHeadersIndexes[Array.from(col.children).indexOf(data)]] = data.textContent;
+                }
+
+            }
+            result.push(rowData);
+        }
+            
+    }
+
+    document.getElementById('output').textContent = JSON.stringify(result);
 }
