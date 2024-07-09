64aa5abe8c8ba3105b9e0ffdde9ef2a6054c207f
xaoccc <xaocccc@gmail.com>
2024-07-09 11:02:40 +0300

    js dom exercise

--- DOM Introduction - Exercise/09. Number Convertor/solution.js
+++ DOM Introduction - Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,34 @@
-function solve() {
 
-    //TODO...
+function solve() {
+    const numberInput = document.getElementById('input').textContent;
+    const selectTo = document.getElementById('selectMenuTo');
+    const binary = document.querySelector('#selectMenuTo option');
+    const hexadecimal = document.createElement('option');
+    const resultField = document.getElementById('result');
+    const convertButton = document.querySelector('#container button');
+
+    binary.id = 'binary';
+    binary.value = 'binary';
+    binary.textContent = 'Binary';
+    
+    hexadecimal.id = 'hexadecimal';
+    selectTo.appendChild(hexadecimal);
+    hexadecimal.value = 'hexadecimal';
+    hexadecimal.textContent = 'Hexadecimal';
+
+    convertButton.addEventListener('click', () => {
+        resultField.textContent = 'test';
+    } )
+   
+
+
+    function binaryConverter () {
+        return;
+    }
+
+    function hexadecimalConverter () {
+        return;
+    }
+
+
 }
