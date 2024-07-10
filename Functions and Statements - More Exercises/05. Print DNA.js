1bdbce0074ae7c2d9b9ab918c2b33eb551a6f1fa
xaoccc <xaocccc@gmail.com>
2024-07-10 12:41:51 +0300

    solution update

--- Functions and Statements - More Exercises/05. Print DNA.js
+++ Functions and Statements - More Exercises/05. Print DNA.js
@@ -1,15 +1,14 @@
 function printDNA(input) {
     const sequence = 'ATCGTTAGGG';
     for (i=0; i<input*2; i+=2) {
+        let [char1, char2] = [sequence[i % sequence.length], sequence[i % sequence.length + 1]]
         if (Math.trunc(i/2) % 4 === 0 || i === 0){
-            console.log(`**${sequence[i % sequence.length ]}${sequence[i % sequence.length+1]}**`);
-        }
-        else if (Math.trunc(i/2) % 2 === 0 && Math.trunc(i/2) % 4 !== 0){
-            console.log(`${sequence[i % sequence.length ]}----${sequence[i % sequence.length+1]}`);
+            console.log(`**${char1}${char2}**`);
+        } else if (Math.trunc(i/2) % 2 === 0 && Math.trunc(i/2) % 4 !== 0){
+            console.log(`${char1}----${char2}`);
         } else {
-            console.log(`*${sequence[i % sequence.length ]}--${sequence[i % sequence.length+1]}*`)
-        }
-        
+            console.log(`*${char1}--${char2}*`)
+        }        
     }
 }
 
