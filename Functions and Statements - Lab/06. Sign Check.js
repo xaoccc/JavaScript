bc526dfe177e4b25bf5373fd9584f051b1a4240e
xaoccc <114498517+xaoccc@users.noreply.github.com>
2024-07-13 10:36:39 +0300

    js problems

--- Functions and Statements - Lab/06. Sign Check.js
+++ Functions and Statements - Lab/06. Sign Check.js
@@ -1,12 +1,8 @@
 function signCheck(numOne, numTwo, numThree ) {
-    negative = (numOne < 0 && numTwo < 0 && numThree < 0) || (numOne > 0 && numTwo > 0 && numThree < 0) || (numOne < 0 && numTwo > 0 && numThree > 0) || (numOne > 0 && numTwo < 0 && numThree > 0)
-    // Spare 4 console logs:
-    if (negative) {
-        console.log('Negative');
-    } else {
-        console.log('Positive');
-    }
+    let negative = (numOne < 0 && numTwo < 0 && numThree < 0) || (numOne > 0 && numTwo > 0 && numThree < 0) || (numOne < 0 && numTwo > 0 && numThree > 0) || (numOne > 0 && numTwo < 0 && numThree > 0);
+
+    (negative) ? console.log('Negative') : console.log('Positive');
+
 }
 
-
-
+signCheck(-5, -12, 3);
