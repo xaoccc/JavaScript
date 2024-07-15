1a179b35ceba536932b2b735916be57853c49e79
xaoccc <xaocccc@gmail.com>
2024-07-15 15:42:37 +0300

    js problems

--- Objects and Classes - More Exercises/12. SoftUni Students.js
+++ Objects and Classes - More Exercises/12. SoftUni Students.js
@@ -4,9 +4,8 @@ function students(input) {
             this.courseName = courseName;
             this.capacity = Number(capacity);
             this.students = [];
-            }
         }
-
+    }
     class Student {
         constructor(userName, email, credits) {
             this.userName = userName;
@@ -31,7 +30,7 @@ function students(input) {
             }
 
         } else if (line.indexOf('email') !== -1) {         
-            const regex = /([a-zA-Z0-9]+)\[([0-9]+)\] with email ([a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z]+) joins ([a-zA-Z#]+)/;            
+            const regex = /(.+)\[(\d+)\] with email (.+) joins (.+)/;            
             const match = line.match(regex);
             if (match) {
                 const userName = match[1];
@@ -56,8 +55,16 @@ function students(input) {
             }
         }
     }
-    console.log(courses);
+
+    courses = courses.sort((a, b) => b.students.length - a.students.length);
+
+    for (let course of courses) {
+        console.log(`${course.courseName}: ${course.capacity} places left`);
+        course.students = course.students.sort((a, b) => b.credits - a.credits);
+        for (let student of course.students) {
+            console.log(`--- ${student.credits}: ${student.userName}, ${student.email}`);
+        }
+    }
+    
 }
 
-students(['JavaBasics: 2', 'user1[25] with email user1@user.com joins C#Basics', 'C#Advanced: 3', 'JSCore: 4', 'user2[30] with email user2@user.com joins C#Basics', 'user13[50] with email user13@user.com joins JSCore', 'user1[25] with email user1@user.com joins JSCore', 'user8[18] with email user8@user.com joins C#Advanced', 'user6[85] with email user6@user.com joins JSCore', 'JSCore: 2', 'user11[3] with email user11@user.com joins JavaBasics', 'user45[105] with email user45@user.com joins JSCore', 'user007[20] with email user007@user.com joins JSCore', 'user700[29] with email user700@user.com joins JSCore', 'user900[88] with email user900@user.com joins JSCore']);
-// students(['JavaBasics: 2', 'JavaBasics: 5', 'C#Advanced: 3', 'JSCore: 4', 'user13[50] with email user13@user.com joins JSCore']);
