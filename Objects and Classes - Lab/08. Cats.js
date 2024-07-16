96bdee5bb3e9a79e94f7376110a9448b7d793bec
xaoccc <xaocccc@gmail.com>
2024-07-16 11:25:01 +0300

    solution update

--- Objects and Classes - Lab/08. Cats.js
+++ Objects and Classes - Lab/08. Cats.js
@@ -10,12 +10,17 @@ function cat(input) {
         }
     }
 
-    let cats = [];
-    for (let i = 0; i < input.length; i++) {
-        let [name, age] = input[i].split(' ');
-        cats.push(new Cat(name, age));
-    }
-
-    cats.forEach(cat => cat.meow());
+    // 1. Use map() 
+    // 1.1. entry is each element in the array
+    // 1.2. We receive an array of arrays, where each element (entry) is split into an array of several elements
+    // 2. Use map() again
+    // 2.1. Use array destructuring [name, age] to get the variables name and age
+    // 2.2. Use the variables name and age as parameters for a new object, instance of class Cat
+    // 2.3. Create object for each [name, age] pair
+    // 2.4. Again, store all the objects in the array
+    // 3. Use forEach()
+    // 3.1. For each cat object we return the class method meow() executed on the object
+    input.map((entry => entry.split(' '))).map(([name, age]) => new Cat(name, age)).forEach((cat) => cat.meow());
 }
 
+// cat(['Candy 1', 'Poppy 3', 'Nyx 2']);
