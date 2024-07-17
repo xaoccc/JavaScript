e2309d5f65f84b1ea29ddee7d11136a37a6bde63
xaoccc <xaocccc@gmail.com>
2024-07-17 16:08:38 +0300

    solved

--- Exam 8 Apr 2023/01. Sprint Review.js
+++ Exam 8 Apr 2023/01. Sprint Review.js
@@ -73,19 +73,3 @@ function sprintReview(input) {
     }
 
 }
-
-sprintReview(  [
-    '4',
-    'Kiril:BOP-1213:Fix Typo:Done:1',
-    'Peter:BOP-1214:New Products Page:In Progress:12',
-    'Mariya:BOP-1215:Setup Routing:ToDo:8',
-    'Georgi:BOP-1216:Add Business Card:Code Review:3',
-    'Add New:Sam:BOP-1237:Testing Home Page:Done:3',
-    'Change Status:Georgi:BOP-1216:Done',
-    'Change Status:Will:BOP-1212:In Progress',
-    'Remove Task:Georgi:3',
-    'Change Status:Mariya:BOP-1215:Done',
-]
-
-
-)
