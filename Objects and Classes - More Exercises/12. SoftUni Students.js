de7187f58b091f150e13b3fea879b6cc471b5369
xaoccc <xaocccc@gmail.com>
2024-07-17 13:17:32 +0300

    better solution

--- Objects and Classes - More Exercises/12. SoftUni Students.js
+++ Objects and Classes - More Exercises/12. SoftUni Students.js
@@ -13,20 +13,16 @@ function students(input) {
             this.credits = credits;
         }
     }
-    let courses = [];
+    let courses = {};
 
     for (let line of input) {
         if (line.indexOf(':') !== -1) {
             let [courseName, capacity] = line.split(': ');  
-            let courseFound = false; 
-            for (let course of courses) {
-                if (course.courseName === courseName) {
-                    course.capacity += Number(capacity);
-                    courseFound = true; 
-                } 
-            }
-            if (!courseFound) {
-                courses.push(new Course(courseName, Number(capacity)));
+            capacity = Number(capacity);
+            if (!courses[courseName]) {
+                courses[courseName] = new Course(courseName, capacity);
+            } else {
+                courses[courseName].capacity += capacity;
             }
 
         } else if (line.indexOf('email') !== -1) {         
@@ -38,33 +34,37 @@ function students(input) {
                 const email = match[3];
                 const courseName = match[4];
 
-                for (let course of courses) {
-                    if (course.courseName === courseName && course.capacity > 0) {
-                        let studentFound = false;
-                        for (let student of course.students) {
-                            if (student.userName === userName) {
-                                studentFound = true;                            
-                            }
-                        }
-                        if (!studentFound) {
-                            course.students.push(new Student(userName, email, credits));
-                            course.capacity -= 1;
-                        }
-                    }
+                if (courses[courseName] && courses[courseName].capacity > courses[courseName].students.length) {
+                    courses[courseName].students.push(new Student(userName, email, credits));
                 }
             }
         }
     }
 
-    courses = courses.sort((a, b) => b.students.length - a.students.length);
 
-    for (let course of courses) {
-        console.log(`${course.courseName}: ${course.capacity} places left`);
-        course.students = course.students.sort((a, b) => b.credits - a.credits);
+
+    let sortedCourses = Object.values(courses).sort((a, b) => b.students.length - a.students.length);
+ 
+    for (let course of sortedCourses) {
+        console.log(`${course.courseName}: ${course.capacity - course.students.length} places left`);
+        course.students.sort((a, b) => b.credits - a.credits);
         for (let student of course.students) {
             console.log(`--- ${student.credits}: ${student.userName}, ${student.email}`);
         }
     }
     
 }
+
+students(['JavaBasics: 15',
+    'user1[26] with email user1@user.com joins JavaBasics',
+    'user2[36] with email user11@user.com joins JavaBasics',
+    'JavaBasics: 5',
+    'C#Advanced: 5',
+    'user1[26] with email user1@user.com joins C#Advanced',
+    'user2[36] with email user11@user.com joins C#Advanced',
+    'user3[6] with email user3@user.com joins C#Advanced',
+    'C#Advanced: 1',
+    'JSCore: 8',
+    'user23[62] with email user23@user.com joins JSCore']
+    );
 
