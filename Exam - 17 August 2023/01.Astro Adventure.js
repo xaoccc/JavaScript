d2f1e566b649031296646b96f76db1124d26676e
xaoccc <xaocccc@gmail.com>
2024-07-19 11:30:45 +0300

    functinal solution success

--- Exam - 17 August 2023/01.Astro Adventure.js
+++ Exam - 17 August 2023/01.Astro Adventure.js
@@ -6,8 +6,7 @@ function astro(input) {
         return acc;
     }, {})
 
-    function findAstronaut(astronautName){
-        
+    function findAstronaut(astronautName){        
         for (let astronaut of data) {
             if (astronautName === astronaut.name) {
                 return astronaut;
@@ -15,11 +14,17 @@ function astro(input) {
         }
     }
     
+    function refill(resource, astronaut, resorceToUpdate, maxValue) {
+        let initialResourceLevel = astronaut[resorceToUpdate];
+        astronaut[resorceToUpdate] += resource;
+        (astronaut[resorceToUpdate] > maxValue) ? astronaut[resorceToUpdate] = maxValue : null;
+        return (astronaut[resorceToUpdate] !== maxValue) ? resource : maxValue - initialResourceLevel; 
+    }
+    
     input.slice(Number(input[0]) + 1).forEach((line) => {
         let [command, astronautName, ...other] = line.split(' - ');
         if (command === 'End') {
-
-            // Print result
+            data.forEach((astronaut) => console.log(`Astronaut: ${astronaut.name}, Oxygen: ${astronaut.oxygenLevel}, Energy: ${astronaut.energyReserves}`))
             process.exit();
         }
         switch (command) {
@@ -34,24 +39,28 @@ function astro(input) {
                     console.log(`${astronautName} does not have enough energy to explore!`);
                 }
                 break;
-            case 'Refuel':
-                // Do something...
+            case 'Refuel':  
+            // Example of functional programming: Functions are First-Class and can be Higher-Order
+            // findAstronaut() is First-Class function and is passed as a parameter to refill()   
+            // refill() is First-Class function as it is treated as a variable passed to console.log()
+            // refill() is Higher-Order because it takes findAstronaut() as a parameter                        
+                console.log(`${astronautName} refueled their energy by ${refill(Number(other[0]), findAstronaut(astronautName), 'energyReserves', 200)}!`);
                 break;
             case 'Breathe':
-                // Do something...
+                console.log(`${astronautName} took a breath and recovered ${refill(Number(other[0]), findAstronaut(astronautName), 'oxygenLevel', 100)} oxygen!`);
                 break;
-
         }
-    })
+    })    
 }
 
 astro([  '3',
     'John 50 120',
     'Kate 80 180',
     'Rob 70 150',
-    'Explore - Kate - 50',
+    'Explore - John - 50',
     'Refuel - Kate - 30',
     'Breathe - Rob - 20',
     'End']
+  
   )
 
