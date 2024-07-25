33278d84e23ef490a3555a0a81d1832eaf82991b
xaoccc <xaocccc@gmail.com>
2024-07-25 14:42:56 +0300

    DOM Manipulation and Events final

--- DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,9 @@
 function validate() {
     console.log('TODO:...');
+    let input = document.querySelector('input');
+    let pattern = /^[a-z]+@\w+.{1}[a-z]+$/
+
+    input.addEventListener('change', function(e) {
+        (!input.value.match(pattern)) ? input.classList.add('error') : input.classList.remove('error');
+    })
 }
--- DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
+++ DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-   //TODO...
+   let addButtons = document.querySelectorAll('.add-product');
+   let checkoutButton = document.querySelector('.checkout');
+   let shoppingCart = document.querySelector('textarea');
+   let [total, items] = [0, []];
+
+   addButtons.forEach((button) => {
+      button.addEventListener('click', function(e) {
+         let productPrice = Number(e.target.parentElement.nextElementSibling.textContent);
+         let productName = e.target.parentElement.previousElementSibling.firstElementChild.textContent;
+
+         shoppingCart.textContent += `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`
+         total += productPrice;
+
+         (items.indexOf(productName) === -1) ?  items.push(productName) : null;
+      })
+   })
+
+   checkoutButton.addEventListener('click', function(e) {
+      addButtons.forEach((button) => button.disabled = true);
+      checkoutButton.disabled = true;
+      shoppingCart.textContent += `You bought ${items.join(', ')} for ${total.toFixed(2)}.`
+   })
 }
