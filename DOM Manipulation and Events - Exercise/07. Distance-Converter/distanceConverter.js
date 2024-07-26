0b87e6c74cd629f4a5d3ab08297a46a5ab4a7e31
xaoccc <xaocccc@gmail.com>
2024-07-26 16:38:06 +0300

    DOM Manipulation and Events exercise

--- DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
+++ DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,24 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const input = document.querySelector('#inputDistance');
+    let resultField = document.querySelector('#outputDistance');
+    const selectFrom = document.querySelectorAll('#inputUnits option');
+    const selectTo = document.querySelectorAll('#outputUnits option');
+    const button = document.querySelector('#convert');
+    
+    let [from, to] = ['', ''];
+    let valueInMeters = 0;
+    const data = { km: 1000, m: 1, cm: 0.01, mm: 0.001, mi: 1609.34, yrd: 0.9144, ft: 0.3048, in: 0.0254 }
+
+    button.addEventListener('click', function(e) {
+        selectFrom.forEach((option) => {
+            (option.selected) ? from = option.value : null;
+        })
+        valueInMeters = data[from] * Number(input.value);
+
+        selectTo.forEach((option) => {
+            (option.selected) ? to = option.value : null;
+        })
+        resultField.value = valueInMeters / data[to]
+    })
+
 }
--- DOM Manipulation and Events - Exercise/08. Sudomu/solution.js
+++ DOM Manipulation and Events - Exercise/08. Sudomu/solution.js
@@ -1,4 +1,63 @@
-function solve() {
+function solve() {    
+    const sumbit = document.querySelector('button');
+    const reset = document.querySelectorAll('button')[1];
+    let result = document.querySelector('#check p');
+    let table = document.querySelector('table');
+    let invalidNum = false;
 
-    //TODO...
+    sumbit.addEventListener('click', function(e) {
+        const inputs = Array.from(document.querySelectorAll('input[type="number"]')).map((input) => Number(input.value));
+
+        // Check if each row has the correct sum and there are no repeating numbers
+        function checkRow(start, end) {
+            let row = inputs.slice(start, end)
+            return row.reduce((a, b) => a + b) === 6 && (new Set(row)).size === row.length;
+        }
+
+        // Check if each column has the correct sum and there are no repeating numbers
+        function checkCol(startIdx) {
+            let row = []
+            for (i=startIdx; i<inputs.length; i+=3 ) {
+                row.push(inputs[i])
+            }
+            return row.reduce((a, b) => a + b) === 6 && (new Set(row)).size === row.length;
+        }
+
+        // Check if any number is outside the acceptable range [1,3]
+        inputs.forEach((num) => {
+            checkNum(num);
+        })
+
+        function checkNum(num) {
+            return (num > 3 || num < 1) ? invalidNum = true : null;
+        }
+
+        inputs.forEach((num) => {
+            checkNum(num);
+        })
+
+        if (checkRow(0, 3) && checkRow(3, 6) && checkRow(6, 9) && checkCol(0) && checkCol(1) && checkCol(2) && !invalidNum){
+            table.style.border = '2px solid green';
+            result.style.color = 'green';
+            result.textContent = 'You solve it! Congratulations!';
+        } else {
+            table.style.border = '2px solid red';
+            result.style.color = 'red';
+            result.textContent = 'NOP! You are not done yet...';
+        }
+
+    })
+
+    reset.addEventListener('click', function(e) {
+        rows = document.querySelectorAll('input[type="number"]');
+        rows.forEach(row => {
+            row.value = '';
+        });
+
+        table.style.border = '';
+        result.style.color = '';
+        result.textContent = '';
+    })
+
+
 }
