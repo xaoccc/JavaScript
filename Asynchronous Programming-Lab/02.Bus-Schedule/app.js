b8cb409dbe0d2c789ab7b4972d711bfb098e7409
xaoccc <114498517+xaoccc@users.noreply.github.com>
2024-08-02 21:03:28 +0300

    bus schedule final

--- Asynchronous Programming-Lab/02.Bus-Schedule/app.js
+++ Asynchronous Programming-Lab/02.Bus-Schedule/app.js
@@ -1,10 +1,31 @@
 function solve() {
+    const departBtn = document.querySelector('#depart');
+    const arrivetBtn = document.querySelector('#arrive');
+    const info = document.querySelector('.info');
+    // The root of the node list
+    let stopId = 'depot';
+    let next = '';
+    
     function depart() {
-        // TODO: 
+        fetch(`http://localhost:3030/jsonstore/bus/schedule/${stopId}`)
+        .then((request) => { 
+            return request.json() 
+        })
+        .then((data) => { 
+            console.log(data);
+            stopId = data.next; 
+            next = data.name;    
+            departBtn.disabled = true;
+            arrivetBtn.disabled = false;
+            info.textContent = `Next stop ${data.name}`;
+        })
+        .catch((error) => console.log('error'));
     }
 
-    async function arrive() {
-        // TODO:
+    async function arrive() { 
+        departBtn.disabled = false;
+        arrivetBtn.disabled = true;
+        info.textContent = `Arriving at ${next}`;        
     }
 
     return {
