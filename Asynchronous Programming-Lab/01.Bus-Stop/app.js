d3d54f92651e2800392f70811fb87ec1742e5c7c
xaoccc <xaocccc@gmail.com>
2024-08-05 09:16:24 +0300

    prmises

--- Asynchronous Programming-Lab/01.Bus-Stop/app.js
+++ Asynchronous Programming-Lab/01.Bus-Stop/app.js
@@ -1,6 +1,32 @@
 function getInfo() {
+    const stopId = document.querySelector('#stopId').value.trim();
+    const stopName = document.querySelector('#stopName');
     const buses = document.querySelector('#buses');
+    Array.from(buses.children).forEach((child) => child.remove())
 
-    fetch('http://localhost:3030/data').then(response => response.json())
+    function createChild(tag, parent, text) {
+        let item = document.createElement(tag);
+        (parent) ? parent.appendChild(item) : null;
+        (text) ? item.textContent = text : null;
+    }
+
+    fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`)
+    .then(response =>  {
+        return response.json();
+    })
+    .then(data => { 
+        stopName.textContent = data.name;
+        Object.entries(data.buses).forEach((entry) => {
+            createChild('li', buses, `Bus ${entry[0]} arrives in ${entry[1]} minutes`);
+        })
+        
+    })
+    .catch((error) => { stopName.textContent = 'Error' });
+
+
+
+
 
 }
+
+
--- js_tests.js
+++ js_tests.js
@@ -1,32 +1,26 @@
-let myObj = {0:'Zdravko', 1:'Pencho', 2:'Gancho'};
 
-let findPencho = Object.entries(myObj).map((entry, i) => `name: ${(myObj[i] === 'Pencho') ? myObj[i] : 'not Pencho'} `);
-console.log(findPencho);
 
-let nums = [12, 34, 45, 61, 5, 2, 14, 67, 453, 235, 76];
 
-let sumNums = nums.reduce((a, b) => a + b, 0);
-console.log(sumNums);
-let sumOdds = nums.reduce((a, b) => a + ((b % 2 !== 0) ? b : 0), 0);
-console.log(sumOdds);
-let sumEven = nums.reduce((a, b) => a + ((b % 2 === 0) ? b : 0), 0);
-console.log(sumEven);
-let sumEvenIndex = nums.map((_, i) => (i % 2 === 0) ? nums[i] : 0).reduce((a, b) => a + b, 0);
-console.log(sumEvenIndex);
-let sumOddIndex = nums.map((_, i) => (i % 2 !== 0) ? nums[i] : 0).reduce((a, b) => a + b, 0);
-console.log(sumOddIndex);
-
-function test(input) {
-    return input.reduce((acc, curr, index) => {
-        if (curr < 2500){
-            let test = {[input[index-1]]: curr + 5000};
-            Object.assign(acc, test);         
-        }
-        
-        return acc;
-        
-        
-    }, {});
+function asyncFn(callback) {
+    setTimeout(() => {
+        callback();
+    }, 8000)
 }
-const result = test(['BMW', 2000, 'Audi', 5000, 'Lada', 500, 'Skoda', 3000]);
-console.log(result)
+
+function anotherAsyncFn(callback) {
+    setTimeout(() => {
+        callback();
+    }, 3500)
+}
+
+
+function oneMoreAsyncFn(callback) {
+    setTimeout(() => {
+        callback();
+    }, -1000)
+}
+
+console.log('start');
+oneMoreAsyncFn(() => console.log('test3'))
+anotherAsyncFn(() => console.log('test2'))
+asyncFn(() => console.log('test1'));
